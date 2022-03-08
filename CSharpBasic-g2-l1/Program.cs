62ad0e2cd87bbe7d178084599b2dadedf31a1920
Elena Stefanovska <64511693+Elijana0@users.noreply.github.com>
2022-03-08 20:32:33 +0100

    Add project files.

---
+++ CSharpBasic-g2-l1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharpBasic_g2_l1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("    *");
+            Console.WriteLine("   ***");
+            Console.WriteLine("  *****");
+            Console.WriteLine(" *******");
+            Console.WriteLine("    *\n   ***\n  *****\n *******");
+
+
+
+            Console.WriteLine("  *");
+            Console.WriteLine("  *");
+            Console.WriteLine(" ***");
+            Console.WriteLine("* * *");
+            Console.WriteLine("  *");
+            Console.WriteLine("  *");
+            Console.WriteLine(" * *");
+            Console.WriteLine("*   *");
+            Console.WriteLine("Write");
+            var input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
